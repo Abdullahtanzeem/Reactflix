e497d53b625d9b490fd5c20007a9ccbef0a38310
Abdullahtanzeem <abdulltanzz313@gmail.com>
2025-02-28 18:24:23 +0530

    added links to my socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,24 +6,24 @@ export const Footer = () => {
     <footer className="bg-white  shadow-sm dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <Link to="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
             <img src={logo} className="h-8" alt="Reactflix Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Reactflix
             </span>
-          </a>
+          </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+              <a href="https://www.instagram.com/haunting_strokes" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
             </li>
             <li>
-              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Facebook</a>
+              <a href="https://www.facebook.com/abdullah.tanzeem.9" target="_blank" className="hover:underline me-4 md:me-6">Facebook</a>
             </li>
             <li>
-              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+              <a href="https://www.linkedin.com/in/abdullah-tanzeem-973b32260/" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
             </li>
             <li>
-              <a href="#" target="_blank" className="hover:underline">Github</a>
+              <a href="https://github.com/Abdullahtanzeem" target="_blank" className="hover:underline">Github</a>
             </li>
           </ul>
         </div>
