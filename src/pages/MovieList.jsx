25eb98dffbae3cc5f1cc58e0b01ecd83acbc9fad
Abdullahtanzeem <abdulltanzz313@gmail.com>
2025-02-28 20:15:59 +0530

    solved netlify error

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,25 +1,20 @@
-
-import { useFetch } from "../hooks/useFetch";
-import { useTitle } from "../hooks/useTitle";
-import { Card } from "../components";
-
-export const MovieList = ({apiPath, title}) => {
-  
-
-  const {data: movies} = useFetch(apiPath);
-
+export const MovieList = ({ apiPath, title }) => {
+  const { data: movies } = useFetch(apiPath);
   const pageTitle = useTitle(title);
-  
+
+  if (!movies) {
+    return <p>Loading...</p>; // Show a loading state
+  }
+
   return (
     <main>
       <section className="py-7 mt-8">
         <div className="flex justify-start flex-wrap other:justify-evenly">
-          {movies.map((movie) => (
+          {movies?.map((movie) => (
             <Card key={movie.id} movie={movie} />
           ))}
-          
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
